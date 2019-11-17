2ba6fcea3bad818f0ac1dab62e0f73c1fb31533c
vg0823 <vidhya.damamkar@gmail.com>
2019-11-16 23:48:14 -0500

    Added edit route

--- app.js
+++ app.js
@@ -63,6 +63,22 @@ app.get("/blogs/:id", function(req,res){
     });
 });
 
+//Edit 
+app.get("/blogs/:id/edit", function(req,res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.render("edit",{blog:foundBlog});
+        }
+    });
+});
+
+//Update route
+app.put("/blogs/:id", function(req,res){
+    res.send("UPDATE ROUTE");
+});
+
 app.listen(port, function(){
     console.log('SERVER IS RUNNING');
 })
