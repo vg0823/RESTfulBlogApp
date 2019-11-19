f131f933d92c1f9f2d97378fc5cb207d4bdb13f1
vg0823 <vidhya.damamkar@gmail.com>
2019-11-18 22:26:54 -0500

    Some more changes related to PUT request and method override

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var bodyParser = require("body-parser"),
-methodOverride = require("method-override"),
 mongoose = require("mongoose"),
 express = require("express"),
 app = express();
@@ -13,7 +12,6 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
-app.use(methodOverride("_method"));
 //MONGOOSE MODEL CONFIG
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -76,7 +74,7 @@ app.get("/blogs/:id/edit", function(req,res){
 });
 
 //Update route
-app.put("/blogs/:id", function(req,res){
+app.post("/blogs/:id/update", function(req,res){
     res.send("UPDATE ROUTE");
 });
 
